8f29ab418bc8a1ab3ae713432598894637ceaa62
UnownPlain <38232575+UnownPlain@users.noreply.github.com>
2022-12-17 20:10:49 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,38 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* google adsense */}
-        {/* <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}",
-            enable_page_level_ads: true
-            });
-            `,
-          }}
-        /> */}
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
